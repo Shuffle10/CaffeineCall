6f5cf8d8168afc74c32200c9704a207bcf07d1b7
Jack <jack.revelett@gmail.com>
2024-05-05 17:56:31 -0500

    Removed snooze functionality and almost all print statements

--- Alarm.py
+++ Alarm.py
@@ -13,18 +13,9 @@ class alarm:
         while True:
             current_time = datetime.datetime.now().strftime("%H:%M")
             if current_time == alarmtime:
-                print("Wake up, your coffee is ready!")
                 sound.play(loops=-1)
-                response = input("Press any key to snooze, or enter 'coffee' to disable the alarm: ")
-                if response.lower() == 'coffee':
-                    print("Alarm disabled, enjoy your coffee")
-                    sound.stop()
-                    break
-                else:
-                    print("Snoozed for 5 minutes")
-                    alarm_time = (datetime.datetime.now() + datetime.timedelta(minutes=snooze_time)).strftime("%H:%M")
-                    print(alarm_time)
-                    time.sleep(snooze_time * 60)
+                response = input("Press enter to disable the alarm: ")
+                sound.stop()
 
             time.sleep(1)
 
--- Distance Sensor.py
+++ Distance Sensor.py
@@ -4,10 +4,21 @@ from gpiozero import DistanceSensor
 
 
 def stop_functionality():
-    return False
+    return True
 
 ultrasonic = DistanceSensor(echo=17, trigger=4, threshold_distance=0.6)
 while True:
     print(ultrasonic.distance)
     ultrasonic.when_out_of_range = stop_functionality
+
+
+
+def check_distance(dist):
+    temp_dist = ultrasonic.distance
+    if temp_dist in range(dist - 0.1, dist + 0.1):
+        return True
+    else:
+        while temp_dist not in range(dist - 0.1, dist + 0.1):
+            temp_dist = ultrasonic.distance
+    return True
 
